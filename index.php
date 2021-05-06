40fd141b53e37d08e14b9ad1e066ba87fe63ff07
Langmann-art <67643178+Langmann-art@users.noreply.github.com>
2021-05-06 17:02:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 $meineVariable = "<b>Text</b>";
 for($i=1; $i<10; $i++){
 echo "Dieser Text wird mit php ausgegeben: ".$meineVariable. "hier geht es wieder weiter ".$meineVariable;
-  echo "\n";
+  echo "<br>";
 }
 ?>
 
