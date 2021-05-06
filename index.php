1b011559f4163c54e08e9205b25d03c42e35358b
Langmann-art <67643178+Langmann-art@users.noreply.github.com>
2021-05-06 17:10:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ for($i=1; $i<10; $i++){
 echo "Dieser Text wird mit php ausgegeben: ".$meineVariable. "hier geht es wieder weiter ".$meineVariable;
   echo "<br>";
 }
-?>
+
 $cars = array("Volvo", "BMW", "Toyota");
 echo count($cars);
+    for($n=0; $n<count($cars); $n++){
+     echo $cars[$n]; 
+    }
+    
+    ?>
   </body>
     </html>
